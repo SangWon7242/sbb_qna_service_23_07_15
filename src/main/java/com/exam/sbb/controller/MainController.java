894b195dc6523c69fe31dc35bb2a9e3046dba861
SangWon7242 <gotkdqja@gmail.com>
2023-07-16 14:37:24 +0900

    modifyArticle 구현

--- src/main/java/com/exam/sbb/controller/MainController.java
+++ src/main/java/com/exam/sbb/controller/MainController.java
@@ -2,6 +2,7 @@ package com.exam.sbb.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -136,7 +138,12 @@ public class MainController {
     return "세션변수 %s의 값은 %s입니다.".formatted(name, value);
   }
 
-  private List<Article> articles = new ArrayList<>();
+  private List<Article> articles = new ArrayList<>(
+      Arrays.asList(
+          new Article("제목", "내용"),
+          new Article("제목", "내용")
+      )
+  );
 
   @GetMapping("/addArticle")
   @ResponseBody
@@ -161,8 +168,30 @@ public class MainController {
     return article;
   }
 
+  @GetMapping("/modifyArticle/{id}")
+  @ResponseBody
+  public String modifyArticle(@PathVariable int id, String title, String body) {
+
+    Article article = articles // id가 1번인 게시물이 앞에서 3번째
+        .stream()
+        .filter(a -> a.getId() == id)
+        .findFirst()
+        .get();
+
+    if(article == null) {
+      return "%d번 게시물은 존재하지 않습니다.".formatted(id);
+    }
+
+    article.setTitle(title);
+    article.setBody(body);
+
+    return "%d번 게시물을 수정하였습니다.".formatted(article.getId());
+  }
+
+
   @AllArgsConstructor
   @Getter
+  @Setter
   class Article {
     private static int lastId = 0;
     private int id;
