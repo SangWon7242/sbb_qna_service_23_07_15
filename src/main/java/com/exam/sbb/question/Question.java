9903942c15269a072c49b83295ece3bf0b8e5bb0
SangWon7242 <gotkdqja@gmail.com>
2023-07-22 14:21:50 +0900

    Answer 엔티티 추가

--- src/main/java/com/exam/sbb/question/Question.java
+++ src/main/java/com/exam/sbb/question/Question.java
@@ -1,8 +1,11 @@
 package com.exam.sbb.question;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
 @Entity // 아래 Question 클래스는 엔티티 클래스이다.
 // 아래 클래스와 1:1로 매칭되는 테이블이 DB 없다면, 자동으로 생성되어야 한다.
 public class Question {
