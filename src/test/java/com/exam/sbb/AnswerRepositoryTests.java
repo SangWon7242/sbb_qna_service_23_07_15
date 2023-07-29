4ed85abe70aaf759c37aa3fbdb543c024e3033fc
SangWon7242 <gotkdqja@gmail.com>
2023-07-29 14:43:31 +0900

    answer 조회

--- src/test/java/com/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -11,6 +11,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 public class AnswerRepositoryTests {
 
@@ -34,6 +36,20 @@ public class AnswerRepositoryTests {
 
   private void createSampleData() {
     QuestionRepositoryTests.createSampleData(questionRepository);
+
+    Question q = questionRepository.findById(1).get();
+
+    Answer a1 = new Answer();
+    a1.setContent("sbb는 질문답변 게시판입니다.");
+    a1.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+    a1.setCreateDate(LocalDateTime.now());
+    answerRepository.save(a1);
+
+    Answer a2 = new Answer();
+    a2.setContent("sbb에서는 주로 스프링관련 내용을 다룹니다.");
+    a2.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+    a2.setCreateDate(LocalDateTime.now());
+    answerRepository.save(a2);
   }
 
   @Test
@@ -45,6 +61,12 @@ public class AnswerRepositoryTests {
     a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
     a.setCreateDate(LocalDateTime.now());
     answerRepository.save(a);
+  }
+
+  @Test
+  void 조회() {
+    Answer a = answerRepository.findById(1).get();
+    assertThat(a.getContent()).isEqualTo("sbb는 질문답변 게시판입니다.");
   }
 
 }
