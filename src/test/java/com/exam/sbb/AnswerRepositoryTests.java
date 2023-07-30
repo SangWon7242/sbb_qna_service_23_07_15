db66dc6a5ecb19c1de6a14ec64789781fd8ce5f1
SangWon7242 <gotkdqja@gmail.com>
2023-07-30 14:25:45 +0900

    양방향 관계 만들어주기

--- src/test/java/com/exam/sbb/AnswerRepositoryTests.java
+++ src/test/java/com/exam/sbb/AnswerRepositoryTests.java
@@ -43,7 +43,6 @@ public class AnswerRepositoryTests {
 
     // 관련 답변이 하나도 없는 상태에서 쿼리 발생
     Question q = questionRepository.findById(1).get();
-    System.out.println("q 1st : " + q);
 
     Answer a1 = new Answer();
     a1.setContent("sbb는 질문답변 게시판입니다.");
@@ -51,11 +50,17 @@ public class AnswerRepositoryTests {
     a1.setCreateDate(LocalDateTime.now());
     answerRepository.save(a1);
 
+    q.getAnswerList().add(a1);
+
     Answer a2 = new Answer();
     a2.setContent("sbb에서는 주로 스프링관련 내용을 다룹니다.");
     a2.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
     a2.setCreateDate(LocalDateTime.now());
     answerRepository.save(a2);
+
+    q.getAnswerList().add(a2);
+
+    questionRepository.save(q);
   }
 
   @Test
@@ -89,9 +94,6 @@ public class AnswerRepositoryTests {
   void question으로부터_관련된_질문들_조회() {
     // SELECT * FROM question WHERE id = 1
     Question q = questionRepository.findById(1).get();
-    System.out.println("q 2nd : " + q);
-
-    q = questionRepository.findById(1).get();
     // DB 연결이 끊김
 
     // SELECT * FROM answer WHERE question_id = 1;
