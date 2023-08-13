94ed3ced7790157100731928be7b33accbd29faa
SangWon7242 <gotkdqja@gmail.com>
2023-08-13 14:53:52 +0900

    SiteUser 엔티티 추가, 다른 엔티티의 주키를 Long 타입으로

---
+++ src/main/java/com/exam/sbb/user/SiteUser.java
@@ -0,0 +1,23 @@
+package com.exam.sbb.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class SiteUser { // class 이름을 user라 해도 상관x, 그런데 스프링 시큐리티는 user라는 클래스가 이미 정의되어 있다.
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true)
+  private String username;
+
+  private String password;
+
+  @Column(unique = true)
+  private String email;
+}
