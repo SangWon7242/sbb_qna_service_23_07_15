ad433f65dc022d7724e6775019161e147e8f0872
SangWon7242 <gotkdqja@gmail.com>
2023-08-13 13:15:36 +0900

    역순 정렬

--- src/main/java/com/exam/sbb/question/QuestionService.java
+++ src/main/java/com/exam/sbb/question/QuestionService.java
@@ -5,9 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class QuestionService {
@@ -20,7 +23,12 @@ public class QuestionService {
   }
 
   public Page<Question> getList(int page) {
-    Pageable pageable = PageRequest.of(page, 10); // 한 페이지에 게시물 10까지 가능
+    List<Sort.Order> sorts = new ArrayList<>();
+    sorts.add(Sort.Order.desc("createDate"));
+    // sorts.add(Sort.Order.desc("id"));
+
+    Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한 페이지에 게시물 10까지 가능
+
     return questionRepository.findAll(pageable);
   }
 
