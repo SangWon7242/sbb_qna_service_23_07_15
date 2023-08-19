c75a944114e45914053c1cdf00e2df93f20650e5
SangWon7242 <gotkdqja@gmail.com>
2023-08-19 14:22:38 +0900

    회원가입시 중복관련 에러가 발생 했을 때, 정확히 어떤 정보 때문에 문제가 발생하는지 체크해서 오류 메시지에 명확히 기술

--- src/main/java/com/exam/sbb/user/UserController.java
+++ src/main/java/com/exam/sbb/user/UserController.java
@@ -2,7 +2,6 @@ package com.exam.sbb.user;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -36,14 +35,12 @@ public class UserController {
     try {
       userService.create(userCreateForm.getUsername(),
           userCreateForm.getEmail(), userCreateForm.getPassword1());
-    } catch (DataIntegrityViolationException e) {
-      e.printStackTrace();
-      bindingResult.rejectValue("username", "usernameDuplicated",
-          "이미 사용중인 username입니다.");
+
+    } catch (SignupUsernameDuplicatedException e) {
+      bindingResult.reject("signupUsernameDuplicated", e.getMessage());
       return "signup_form";
-    } catch(Exception e) {
-      e.printStackTrace();
-      bindingResult.reject("signupFailed", "이미 등록된 사용자입니다.");
+    } catch(SignupEmailDuplicatedException e) {
+      bindingResult.reject("signupEmailDuplicated", e.getMessage());
       return "signup_form";
     }
 
