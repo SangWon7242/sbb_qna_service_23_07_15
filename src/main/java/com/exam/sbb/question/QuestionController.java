5d40c8724da6505efb87aa5e7a0f37518cba15e9
SangWon7242 <gotkdqja@gmail.com>
2023-08-27 12:37:31 +0900

    질문 수정 화면

--- src/main/java/com/exam/sbb/question/QuestionController.java
+++ src/main/java/com/exam/sbb/question/QuestionController.java
@@ -1,5 +1,6 @@
 package com.exam.sbb.question;
 
+import com.exam.sbb.DataNotFoundException;
 import com.exam.sbb.answer.AnswerForm;
 import com.exam.sbb.user.SiteUser;
 import com.exam.sbb.user.UserService;
@@ -7,11 +8,13 @@ import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -68,5 +71,23 @@ public class QuestionController {
 
     questionService.create(questionForm.getSubject(), questionForm.getContent(), siteUser);
     return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
+  }
+
+  @PreAuthorize("isAuthenticated()")
+  @GetMapping("/modify/{id}")
+  public String questionModify(QuestionForm questionForm, @PathVariable("id") Integer id, Principal principal) {
+    Question question = this.questionService.getQuestion(id);
+
+    if(question == null) {
+      throw new DataNotFoundException("%d번 질문은 존재하지 않습니다.".formatted(id));
+    }
+
+    if(!question.getAuthor().getUsername().equals(principal.getName())) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
+    }
+
+    questionForm.setSubject(question.getSubject());
+    questionForm.setContent(question.getContent());
+    return "question_form";
   }
 }
--- src/main/java/com/exam/sbb/question/QuestionService.java
+++ src/main/java/com/exam/sbb/question/QuestionService.java
@@ -26,7 +26,7 @@ public class QuestionService {
   public Page<Question> getList(int page) {
     List<Sort.Order> sorts = new ArrayList<>();
     sorts.add(Sort.Order.desc("createDate"));
-    // sorts.add(Sort.Order.desc("id"));
+    sorts.add(Sort.Order.desc("id"));
 
     Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts)); // 한 페이지에 게시물 10까지 가능
 
