88ddfc2f118eefa2e1d96d48a54c0ce10d585e20
SangWon7242 <gotkdqja@gmail.com>
2023-08-27 12:43:28 +0900

    질문 수정 폼 처리

--- src/main/java/com/exam/sbb/question/QuestionService.java
+++ src/main/java/com/exam/sbb/question/QuestionService.java
@@ -47,4 +47,12 @@ public class QuestionService {
 
     this.questionRepository.save(q);
   }
+
+  public void modify(Question question, String subject, String content) {
+    question.setSubject(subject);
+    question.setContent(content);
+    question.setModifyDate(LocalDateTime.now());
+
+    this.questionRepository.save(question);
+  }
 }
