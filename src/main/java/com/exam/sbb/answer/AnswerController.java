738adf9f55e65f80d57f5baa046ffe68aa2e8837
SangWon7242 <gotkdqja@gmail.com>
2023-09-02 13:06:20 +0900

    답변 작성 후, 다시 돌아온 페이지에서 새로 생성된 답변으로 스크롤 이동

--- src/main/java/com/exam/sbb/answer/AnswerController.java
+++ src/main/java/com/exam/sbb/answer/AnswerController.java
@@ -44,10 +44,10 @@ public class AnswerController {
     SiteUser siteUser = userService.getUser(principal.getName());
     
     // 답변 등록 시작
-    answerService.create(question, answerForm.getContent(), siteUser);
+    Answer answer = answerService.create(question, answerForm.getContent(), siteUser);
     // 답변 등록 끝
     
-    return String.format("redirect:/question/detail/%s", id);
+    return String.format("redirect:/question/detail/%s#answer_%d", id, answer.getId());
   }
 
   @PreAuthorize("isAuthenticated()")
