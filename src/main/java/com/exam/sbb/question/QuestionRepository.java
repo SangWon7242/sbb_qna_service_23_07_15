c59a0ef0669d83bdeefcb1b315a18fe8af4643c8
SangWon7242 <gotkdqja@gmail.com>
2023-09-03 13:38:37 +0900

    kw 파라미터가 질문제목 + 질문내용 + 질문 작성자명 + 답변 내용 검색어로 작동, 중복제거

--- src/main/java/com/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/exam/sbb/question/QuestionRepository.java
@@ -25,7 +25,7 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>, Re
 
   Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContains(String kw, String kw_, String kw__, Pageable pageable);
 
-  Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
+  Page<Question> findDistinctBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
 
   @Modifying
   @Transactional
