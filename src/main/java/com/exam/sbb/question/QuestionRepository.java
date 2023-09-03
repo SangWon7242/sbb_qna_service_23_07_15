16df91389979bc43e69801107cf481e2b3ca11f5
SangWon7242 <gotkdqja@gmail.com>
2023-09-03 13:34:41 +0900

    kw 파라미터가 질문제목 + 질문내용 + 질문 작성자명 + 답변 내용 검색어로 작동

--- src/main/java/com/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/exam/sbb/question/QuestionRepository.java
@@ -25,6 +25,8 @@ public interface QuestionRepository extends JpaRepository<Question, Integer>, Re
 
   Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContains(String kw, String kw_, String kw__, Pageable pageable);
 
+  Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
+
   @Modifying
   @Transactional
   @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
